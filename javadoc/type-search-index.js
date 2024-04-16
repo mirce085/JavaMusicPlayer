2d59a9a4ef6769a6f79fc428d97d92a54c299ea3
CodeDynasty05 <qenizadenazim@gmail.com>
2024-04-16 18:54:18 +0400

    update

---
+++ javadoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"<Unnamed>","l":"CustomCellRenderer"},{"p":"<Unnamed>","l":"Main"},{"p":"<Unnamed>","l":"MusicPlayer"},{"p":"<Unnamed>","l":"MyFirstForm"},{"p":"<Unnamed>","l":"PlayerState"},{"p":"<Unnamed>","l":"Song"},{"p":"<Unnamed>","l":"SongService"}]
